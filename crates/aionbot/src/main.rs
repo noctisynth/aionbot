bcec5ba37793a91c33f49fc0f95e61715f1f3e20
苏向夜 <fu050409@163.com>
2024-10-26 19:12:32 +0800

    chore: remove `bot_into`

--- crates/aionbot/src/main.rs
+++ crates/aionbot/src/main.rs
@@ -16,25 +16,10 @@ pub async fn hello_world(event: Arc<Box<dyn Event>>) -> Result<String> {
     Ok(())
 }
 
-#[register(router = StartsWithRouter::new("/bot"))]
-pub async fn bot_info(event: Arc<Box<dyn Event>>) -> Result<String> {
-    event
-        .reply(
-            r#"NoctisBot v0.1.0-alpha.0 [Rust edition 2024 for AionBot v0.1.0-alpha.0]
-This project is open-source and licensed under the NOL (Normalcy Open License).
-Type [.help] to show more information."#,
-            //             r#"NoctisBot v0.1.0-alpha.0 [Rust v1.83.0-nightly for Aionbot v0.1.0-alpha.0]
-            // 此项目以常态开源协议(NOL)开放许可证的形式开源.
-            // 输入[.help]以获取更多帮助信息."#,
-        )
-        .await?;
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() {
     aionbot_core::runtime::Builder::<OnebotRuntime>::default()
-        .invoke_handler(vec![hello_world(), bot_info()])
+        .invoke_handler(vec![hello_world()])
         .manage(State {})
         .run()
         .await
