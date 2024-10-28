8e50956797ca2d43e69d8a75ebe96188f9c98376
苏向夜 <fu050409@163.com>
2024-10-28 10:49:36 +0800

    feat(aionbot): optimize method export

---
+++ .changes/optimze-export.md
@@ -0,0 +1,6 @@
+---
+"aionbot": patch:feat
+"aionbot-macros": patch:feat
+---
+
+Optimize method export in `aionbot` crate.
--- crates/aionbot/src/prelude.rs
+++ crates/aionbot/src/prelude.rs
@@ -1,9 +1,4 @@
-pub extern crate aionbot_core;
-pub extern crate aionbot_macros;
-
+pub use crate::logger::setup_logger;
 pub use aionbot_core::prelude::*;
 pub use aionbot_core::runtime::Builder;
 pub use aionbot_macros::register;
-
-pub mod logger;
-pub mod prelude;
